32a3ab738d527c51d4de314381377d8c0196562a
KaderCreates <71582048+KaderCreates@users.noreply.github.com>
2021-06-01 16:34:47 -0700

    Redesign and clean up.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,61 +9,39 @@ import Navbar from '../Navbar';
 import SideMenu from '../SideMenu';
 import Preorder from '../Preorder';
 import Sneakers from '../Sneakers';
+import ShopCollectionButton from '../Home/components/ShopCollectionButton';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { Slide } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import { Link } from 'react-router-dom';
 
 
 const styles = {
-  h1: {
-    textAlign: 'center',
-    paddingTop: '1rem'
-  },
-  h3: {
-    color: 'grey'
-  },
-  newsletter: {
-    textAlign: 'center',
-    padding: '1.5rem',
-  },
-  button: {
-    fontSize: '10px',
-    color: 'black',
-    backgroundColor: '#ff726f',
-    padding: '3.2ch', 
-  },
-  footer: {
-
+  img: {
+    width: '100%',
+    textAlign: 'center'
   }
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(0),
-      width: '25ch',
-    } 
-  },
-}));
-
 const Home = () => {
-  const classes = useStyles();
-
   return (
     <div>
-      <header className="App-header">
-
-        <Slideshow />
-        <Navbar />
-      </header>
-      <h1 style={styles.h1}>Subscribe to our newsletter</h1>
-      <h3 style={styles.h3}>Be first to know when pre-orders and exclusive products go live</h3>
-      <div style={styles.newsletter}> 
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic"  variant="outlined" placeholder="Email Address"/>
-        <Button style={styles.button} variant="outlined">Subscribe</Button>
-      </form>
+      <Navbar />
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        alignContent="center"
+      >
+        <div style={styles.img} className="HomeSlideImageContainer">
+          <Slideshow />
+          <ShopCollectionButton />
+        </div>
+      </Box>
+      <div>
       </div>
-      {/* <div id="footer">footer</div> */}
     </div>
   );
 
 }
-export default withStyles(styles)(Home);
+export default withStyles()(Home);
