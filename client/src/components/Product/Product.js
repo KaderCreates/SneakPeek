97a2e6784a10f02fe7cdb26effc1833a23bd771d
KaderCreates <71582048+KaderCreates@users.noreply.github.com>
2021-06-01 16:37:14 -0700

    Redesign, Formatting Size now added

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import Navbar from '../Navbar';
 import Link from '@material-ui/core/Link';
 import ControlledOpen from './components/controlledOpen';
@@ -7,13 +8,24 @@ import './styles.css'
 import Cart from './components/Cart';
 import AddCartButton from './components/addToCart';
 import products from '../../products.js'
+import { relative } from 'path';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
+class Product extends Component {
 
-export default class Product extends Component {
+  style = {
+    container: {
+      position: 'relative',
+      marginTop: '2rem',
+    }
+  }
+
   constructor(props) {
     super(props);
     this.state = {
-
+      size: 0
     }
 
     this.getProduct = this.getProduct.bind(this)
@@ -21,26 +33,36 @@ export default class Product extends Component {
 
   getProduct = () => {
     return products.filter((product) => {
-      return this.props.match.params.id === product.id
+      return parseInt(this.props.match.params.id) === product.id
     }).pop()
   }
+  setSize = (size) => {
+    this.setState({ ...this.state, size })
+  }
   render() {
-    const product = this.getProduct()
+    let product = this.getProduct()
+    product = { ...this.getProduct(), size: this.state.size }
     console.log(product)
     return (
       <>
         <Navbar />
-        <img id='img' src={product.image} alt="imageMapped" />
-        <div id='container'>
-          <h3 >{product.title}</h3>
-          <p >{product.price}</p>
-          <ControlledOpen />
-          <AddCartButton
-            product={product.id}
-          />
-        </div>
+        <CssBaseline />
+        <Container style={this.style.container} >
+          <img id='img' src={product.image} alt="imageMapped" />
+          <div id='container'>
+            <h3 >{product.title}</h3>
+            <p >{product.price}</p>
+            <ControlledOpen setSize={this.setSize} />
+            <AddCartButton
+              product={product.id}
+              cartData={product}
+            />
+          </div>
+        </Container>
       </>
     )
   }
 
 }
+
+export default withRouter(Product);
