254eb3d336185d576b7d3e99ca2d0f1743c21655
KaderCreates <71582048+KaderCreates@users.noreply.github.com>
2021-06-01 16:36:38 -0700

    Redesign

--- client/src/components/Preorder/Preorder.js
+++ client/src/components/Preorder/Preorder.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Navbar from '../Navbar';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 
-class Preorder extends Component {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(0),
+      width: '25ch',
+    }
+  }
+}));
 
-  renderView() {
-    return (
-      <>
-      <div>
-       <h1>Pre Order Page</h1>
-      </div>
-      <Navbar />
-      </>
-    );
-}
+const styles = {
+  h1: {
+    textAlign: 'center',
+    paddingTop: '1rem'
+  },
+  h3: {
+    color: 'grey'
+  },
+  newsletter: {
+    textAlign: 'center',
+    padding: '1.5rem',
+  },
+  button: {
+    fontSize: '10px',
+    color: 'black',
+    backgroundColor: '#ff726f',
+    padding: '3.2ch',
+  },
+  footer: {
 
-  render(){
-    return(
-      <>
-        {this.renderView()}
-      </>
-    )
   }
 }
+
+const Preorder = () => {
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Navbar />
+      <h1 style={styles.h1}>Subscribe to our newsletter</h1>
+      <h3 style={styles.h3}>Be first to know when pre-orders and collection go live</h3>
+      <div style={styles.newsletter}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="outlined-basic" variant="outlined" placeholder="Email Address" />
+          <Button style={styles.button} variant="outlined">Subscribe</Button>
+        </form>
+      </div>
+    </div>
+
+  )
+}
+
 
 export default Preorder
