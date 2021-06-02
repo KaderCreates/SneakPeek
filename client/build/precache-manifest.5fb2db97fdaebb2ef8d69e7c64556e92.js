35a9769c8e8456fba74d54f7c4882a1f1e676330
KaderCreates <71582048+KaderCreates@users.noreply.github.com>
2021-06-02 12:29:33 -0700

    build

---
+++ client/build/precache-manifest.5fb2db97fdaebb2ef8d69e7c64556e92.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b192498e7ea79ae173676a97cb978514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c382787d2be2936d46b",
+    "url": "/static/css/main.7ebc0b4f.chunk.css"
+  },
+  {
+    "revision": "82f9bb25b7ce2440dea5",
+    "url": "/static/js/2.ea845e6e.chunk.js"
+  },
+  {
+    "revision": "4eec2d6e1e496633d8d2813772176424",
+    "url": "/static/js/2.ea845e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c382787d2be2936d46b",
+    "url": "/static/js/main.f7f21ee8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a7851894586756f8c5c0fe465e85925f",
+    "url": "/static/media/Air-Jordan-11-Retro-Cap-And-Gown-Product.a7851894.jpeg"
+  },
+  {
+    "revision": "197fb005877c084a4ab286ed66370509",
+    "url": "/static/media/Air-Jordan-11-Retro-Concord-2018-1.197fb005.jpeg"
+  },
+  {
+    "revision": "e3a6bf82a07d1ed79a16cf4b924cdaa1",
+    "url": "/static/media/Air-Jordan-12-Retro-Black-Red-Flu-Game-2016-Product.e3a6bf82.webp"
+  },
+  {
+    "revision": "4beae8ed3fcc538266a22b8e2ce42605",
+    "url": "/static/media/Air-Jordan-12-Retro-Reverse-Flu-Game-Product.4beae8ed.webp"
+  },
+  {
+    "revision": "7a9d1fcfe7651ce7066384326962fedf",
+    "url": "/static/media/Air-Jordan-12-Retro-The-Master-Product.7a9d1fcf.jpeg"
+  },
+  {
+    "revision": "8b95dc3ad239033d52e456c6e9731f44",
+    "url": "/static/media/AirJordan11RetroConcord2018.8b95dc3a.jpeg"
+  },
+  {
+    "revision": "842606ec668f2acca0a986a8d693a3d7",
+    "url": "/static/media/AirJordan11RetroSpaceJam2016.842606ec.jpeg"
+  },
+  {
+    "revision": "1853b1469686c6c5d2a0976e8b2bc07a",
+    "url": "/static/media/AirJordan6RetroInfared2019.1853b146.jpeg"
+  },
+  {
+    "revision": "0e2bbd2502f534a4929e22894fe0db47",
+    "url": "/static/media/airjordan12frenchblues.0e2bbd25.jpeg"
+  },
+  {
+    "revision": "eaeefc6bcbc3d9548f91dbcc7ae38dea",
+    "url": "/static/media/retrofiba.eaeefc6b.jpeg"
+  },
+  {
+    "revision": "b758d54ca526e794c5a18090184a2b4e",
+    "url": "/static/media/reversetaxi.b758d54c.jpeg"
+  }
+]);
