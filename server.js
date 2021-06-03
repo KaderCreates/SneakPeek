cf143f96f79c20192e89c8f8a826aacc9bf8cda6
KaderCreates <71582048+KaderCreates@users.noreply.github.com>
2021-06-03 11:34:59 -0700

    main

--- server.js
+++ server.js
@@ -30,8 +30,10 @@ router.post('/contact', login.contact)
 app.use('/api', router);
 app.use(cors());
 
+let URI = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_URI : process.env.REACT_APP_PROD_URI
 
-app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/sneakpeek', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
